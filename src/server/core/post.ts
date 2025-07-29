b6a226c793c6520db1256134fe4c2cfef3c61689
Marcus Wood <marcus.wood@reddit.com>
2025-07-29 14:08:36 -0400

    splash screen

--- src/server/core/post.ts
+++ src/server/core/post.ts
@@ -1,28 +1,17 @@
-import { Devvit } from "@devvit/public-api";
-import { context } from "@devvit/server";
-import { reddit } from "@devvit/reddit";
+import { context } from '@devvit/server';
+import { reddit } from '@devvit/reddit';
 
 export const createPost = async () => {
   const { subredditName } = context;
   if (!subredditName) {
-    throw new Error("subredditName is required");
+    throw new Error('subredditName is required');
   }
 
   return await reddit.submitPost({
-    userGeneratedContent: {
-      text: "Hello there! This is a post from a Devvit app",
+    splash: {
+      appDisplayName: '<% name %>',
     },
     subredditName: subredditName,
-    title: "New Post",
-    preview: Devvit.createElement("blocks", {
-      height: "tall",
-      children: [
-        Devvit.createElement("vstack", {
-          height: "100%",
-          width: "100%",
-          backgroundColor: "#ffbf0b",
-        }),
-      ],
-    }),
+    title: '<% name %>',
   });
 };
